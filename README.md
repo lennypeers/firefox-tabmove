c18f2fa9eb51a31743313ac0821b5e61783f5b0a
lennypeers <lenny.peers@gmail.com>
2020-12-29 12:56:49 +0100

    cross browser update
    now compatible with Chrome

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # firefox-tabmove
 
-move the current tab with alt+shift+n (n in [1-9])
+# general
+
+`alt+shift+{1,2,3,4}` keybinds move the tab to the `{first index, left, right, last index}`
+
+This extension should be compatible with latest versions of Firefox and Chrome.
+
+# install
+
+- Firefox:
+	- download the xpi file in the Releases section
+
+- Chrome based browsers:
+	- clone the repository
+	- open `chrome://extensions/`
+	- turn the developper mode on (top right corner)
+	- load the module with `Load unpacked`
+
+# TODO
+- make it available on the extension stores.
--- foo.js
+++ foo.js
@@ -1,9 +1,23 @@
-browser.commands.onCommand.addListener(async function(command) {
-  const target_index = command.slice(-1) % 9 - 1;
-	const sbat = await browser.tabs.query({currentWindow: true, active: true});
-	let current = sbat[0]
-  var moving = browser.tabs.move(current.id, {index: target_index});
-  moving.then(onMoved, onError);
+chrome.commands.onCommand.addListener(async function(command) {
+  await chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+    chrome.tabs.query({ currentWindow: true}, function(sbat) {
+      let targetIndex = 0;
+      switch(command.slice(-1)) {
+        case '1':
+          break;
+        case '4':
+          targetIndex = -1;
+          break;
+        case '2':
+          targetIndex = sbat.findIndex(i => i.id === tabs[0].id) - 1;
+          break;
+        case '3':
+          targetIndex = (sbat.findIndex(i => i.id === tabs[0].id) + 1) % sbat.length;
+          break;
+      }
+      chrome.tabs.move(tabs[0].id, {index: targetIndex});
+    });
+  });
 });
 
 // vim: set ts=2 sts=2 sw=2 et:
